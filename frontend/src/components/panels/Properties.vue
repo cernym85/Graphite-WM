2f37110e642c9ef252e43f92f7e9514c824e9664
psvoboda <psvoboda@myscada.org>
2023-01-29 10:48:43 +0100

    predavani textu - parsujeme mnozstvi zadavanych tagu

--- frontend/src/components/panels/Properties.vue
+++ frontend/src/components/panels/Properties.vue
@@ -53,82 +53,88 @@ export default defineComponent({
 			let processed = false;
 			let type = "";
 			try {
-				let layouty:any=this.propertiesOptionsLayout.layout[0];
+				let layouty: any = this.propertiesOptionsLayout.layout[0];
 				const array = layouty.rowWidgets;
 				for (const obj of array) {
 					if (obj.props.kind === "TextLabel") {
 						type = obj.props.value;
-					}
-					else if (obj.props.kind === "TextInput") {
+					} else if (obj.props.kind === "TextInput") {
 						let tab = window.document.getElementById("__myscadaSelectPanelIdForActive");
 						let children = tab?.getElementsByClassName("tab");
 						if (children && children.length > 1) {
-							let action="id";
+							let action = "id";
 
-							if (!children[1].classList.contains("active")){
-								action="memoryId"
+							if (!children[1].classList.contains("active")) {
+								action = "memoryId";
 							}
 
 							let pb = tab?.getElementsByClassName("panel-body");
 							if (pb && pb.length > 0) {
 								let rect = pb[0].getBoundingClientRect();
-								window.parent.postMessage(JSON.stringify({ type: "selection", action: action, id: obj.props.value, element: type, x: rect.x, y: rect.y, w: rect.width, h: rect.height }), "*");
+								window.parent.postMessage(
+									JSON.stringify({ type: "selection", action: action, id: obj.props.value, element: type, x: rect.x, y: rect.y, w: rect.width, h: rect.height }),
+									"*"
+								);
 							}
-						}
-						else {
+						} else {
 							window.parent.postMessage(JSON.stringify({ type: "selection", action: "hide" }), "*");
 						}
 						processed = true;
 						break;
 					}
 				}
-			} catch (e) { }
+			} catch (e) {}
 
 			if (!processed) {
 				window.parent.postMessage(JSON.stringify({ type: "selection", action: "hide" }), "*");
 			}
 		});
 
-		
 		this.editor.subscriptions.subscribeJsMessage(UpdatePropertyPanelSectionsLayout, (updatePropertyPanelSectionsLayout) => {
 			patchWidgetLayout(this.propertiesSectionsLayout, updatePropertyPanelSectionsLayout);
 			//this.propertiesSectionsLayout = updatePropertyPanelSectionsLayout;
 			//detekce zmeny velikosti kanvasu
-			try{
-			let pom:any=this.propertiesSectionsLayout;
-			let obj:any=pom.layout[0];
-			if (obj.layout && obj.layout.length>0){
-				if (obj.name==="Artboard"){
-					let w=-1;
-					let h=-1;
-					let x=-1;
-					let y=-1;
-					for (const item of obj.layout[0].rowWidgets){
-						if (item.props.kind==="NumberInput" && x===-1){
-							x=item.props.value;
+			try {
+				let pom: any = this.propertiesSectionsLayout;
+				for (const obj of pom.layout) {
+					//let obj: any = pom.layout[0];
+					if (obj.layout && obj.layout.length > 0) {
+						if (obj.name === "Artboard") {
+							let w = -1;
+							let h = -1;
+							let x = -1;
+							let y = -1;
+							for (const item of obj.layout[0].rowWidgets) {
+								if (item.props.kind === "NumberInput" && x === -1) {
+									x = item.props.value;
+								} else if (item.props.kind === "NumberInput" && y === -1) {
+									y = item.props.value;
+								}
+							}
+							for (const item of obj.layout[1].rowWidgets) {
+								if (item.props.kind === "NumberInput" && w === -1) {
+									w = item.props.value;
+								} else if (item.props.kind === "NumberInput" && h === -1) {
+									h = item.props.value;
+								}
+							}
+							if (w >= 0 && h >= 0) {
+								let data = JSON.stringify({ type: "canvasSize", w: w, h: h, x: x, y: y });
+								window.parent.postMessage(data, "*");
+							}
+						} else if (obj.name === "Font") {
+							for (const item of obj.layout[0].rowWidgets) {
+								if (item.props.kind === "TextAreaInput") {
+									let text = item.props.value;
+									let data = JSON.stringify({ type: "textValue", text: text });
+									window.parent.postMessage(data, "*");
+								}
+							}
 						}
-						else if (item.props.kind==="NumberInput" && y===-1){
-							y=item.props.value;
-						}
-					}
-					for (const item of obj.layout[1].rowWidgets){
-						if (item.props.kind==="NumberInput" && w===-1){
-							w=item.props.value;
-						}
-						else if (item.props.kind==="NumberInput" && h===-1){
-							h=item.props.value;
-						}
-					}
-					if (w>=0 && h>=0){
-						let data = JSON.stringify({ type: "canvasSize", w: w, h: h, x: x, y: y });
-						window.parent.postMessage(data, "*");
 					}
 				}
-			}
-			}
-			catch (e){}
+			} catch (e) {}
 		});
-		
 	},
 	components: {
 		LayoutCol,
